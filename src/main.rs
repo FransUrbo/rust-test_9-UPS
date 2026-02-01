fb3f4f0a6e2402651a6fcc87334e8a380f6e96ba
FransUrbo <turbo@bayour.com>
2026-02-01 19:54:32 +0000

    Don't get the config here, let `parse_config()` do this for us.
    + Enable the power save mode.

--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,18 @@ async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let mut i2c_bus = UpsHat::new(p.I2C1, Irqs, p.PIN_6, p.PIN_7, 0x43);
 
-    let config = i2c_bus.get_config().await;
     let shunt = i2c_bus.get_shunt_voltage().await;
     let bus = i2c_bus.get_bus_voltage().await;
     let power = i2c_bus.get_power().await;
     let current = i2c_bus.get_current().await;
     let charge = i2c_bus.get_charge().await;
 
-    info!("Config: {}", config);
     info!("Shunt Voltage: {}mV", shunt as u16);
-    info!("Bus Voltage: {}V", bus);
-    info!("Power: {}V", power);
-    info!("Current: {}V", current);
-    info!("Charge: {}%", charge);
+    info!("Bus Voltage:   {}V", bus);
+    info!("Power:         {}V", power);
+    info!("Current:       {}V", current);
+    info!("Charge:        {}%", charge);
 
-    i2c_bus.set_power_save(false).await;
+    i2c_bus.parse_config().await;
+    i2c_bus.set_power_save(true).await;
 }
